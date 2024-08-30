6dfc42b41cac25df06533050f4d366002de26ec6
83metra <jumo_004@mail.ru>
2024-08-30 12:48:03 +0200

    Update Module 10 3.py
    Добавлено условие, завершающее корректно программу при недостатке на балансе - строки 35 и 36.

--- Module 10 3.py
+++ Module 10 3.py
@@ -4,10 +4,13 @@ class Bank:
     def __init__(self):
         self.balance = 0
         self.lock = threading.Lock()
+        self.count = 0
+        self.number_of_transactions = 100
 
     def deposit(self):
-        for i in range(100):
-            print('Операция по внесению денег № %s' % (i))
+        for i in range(self.number_of_transactions):
+            print('Операция по внесению денег № %s' % (i+1))
+            self.count += 1
             self.money = random.randrange(50, 500)
             if self.balance >= 500 and self.lock.locked():
                 self.lock.release()
@@ -18,20 +21,25 @@ class Bank:
             else:
                 self.balance += self.money
             print( f'Пополнение: {self.money}. Баланс: {self.balance}.')
-            #time.sleep(0.1)
+
+            #time.sleep(0.001)
 
     def take(self):
-        for i in range(100):
-            print('Операция по снятию денег № %s' %(i))
+        for i in range(self.number_of_transactions):
+            print('Операция по снятию денег № %s' %(i+1))
             self.money = random.randrange(50, 500)
             print('Запрос на: %s' %(self.money))
             if self.money <= self.balance:
                 self.balance = self.balance - self.money
                 print('Снятие: {}. Баланс: {}'.format(self.money, self.balance))
+            elif self.count == self.number_of_transactions:
+                print('Запрос отклонён, недостаточно средств, поступление средств в банкомат в ближайшее время не ожидается!')
+                continue
             elif self.money > self.balance:
                 print('Запрос отклонён, недостаточно средств')
+                time.sleep(0.001)
                 self.lock.acquire()
-            #time.sleep(0.1)
+                #time.sleep(0.1)
 
 
 bk = Bank()
